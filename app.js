b15335d8d5db52598093cf1a4dc7654efc7eeb60
Lizz <lizwesterband@gmail.com>
2020-07-15 14:03:47 -0700

    [arrays] Add favorite musicals

---
+++ app.js
@@ -0,0 +1,12 @@
+var name = "Lizz";
+console.log(name);
+
+var loc= "Orange county";
+console.log(loc);
+
+var favoriteNumber= 7;
+console.log(favoriteNumber);
+
+
+var musicals = ["Beetlejuice", "Hamilton"," Lion King", "Matilda", "Shrek"]
+console.log(musicals);
