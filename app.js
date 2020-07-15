00612ff3cf5ff24ec4a5e317c955533ae52d81bc
Lizz <lizwesterband@gmail.com>
2020-07-15 14:22:41 -0700

    [loops] Add while loops

--- app.js
+++ app.js
@@ -10,3 +10,24 @@ console.log(favoriteNumber);
 
 var musicals = ["Beetlejuice", "Hamilton"," Lion King", "Matilda", "Shrek"]
 console.log(musicals);
+
+
+// while loops
+var num = 10;
+var otherNum = 1;
+while(otherNum < num){
+    console.log(otherNum);
+    otherNum += 1;
+}
+
+console.log("--------------")
+
+var numTwo= 20;
+var numThree = 10;
+
+while( numThree < numTwo){
+    console.log(numThree);
+    numThree += 1;
+}
+
+console.log("--------------")
